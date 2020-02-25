9608b32ec968e58dd47225b7cf4e0e1553122610
wumou.wm <wumou.wm@alibaba-inc.com>
2020-02-25 22:25:49 +0800

    + DemoMicroService controller create deployment

--- controllers/demomicroservice_controller.go
+++ controllers/demomicroservice_controller.go
@@ -17,8 +17,10 @@ package controllers
 
 import (
 	"context"
-
 	"github.com/go-logr/logr"
+	appv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -37,11 +39,62 @@ type DemoMicroServiceReconciler struct {
 // +kubebuilder:rbac:groups=devops.my.domain,resources=demomicroservices/status,verbs=get;update;patch
 
 func (r *DemoMicroServiceReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("demomicroservice", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("demomicroservice", req.NamespacedName)
 
-	// your logic here
+	dms := &devopsv1.DemoMicroService{}
+	if err := r.Get(ctx, req.NamespacedName, dms); err != nil {
+		if err := client.IgnoreNotFound(err); err == nil {
+			log.Info("此时没有找到对应的 DemoMicroService resource, 即此处进入了 resource 被删除成功后的生命周期")
+			return ctrl.Result{}, nil
+		} else {
+			log.Error(err, "不是未找到的错误，那么就是意料之外的错误，所以这里直接返回错误")
+			return ctrl.Result{}, err
+		}
+	}
 
+	log.Info("走到这里意味着 DemoMicroService resource 被找到，即该 resource 被成功创建，进入到了可根据该 resource 来执行逻辑的主流程")
+	podLabels := map[string]string{
+		"app": req.Name,
+	}
+	deployment := appv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Deployment",
+			APIVersion: "apps/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      req.Name,
+			Namespace: req.Namespace,
+		},
+		Spec: appv1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: podLabels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: podLabels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:            req.Name,
+							Image:           dms.Spec.Image,
+							ImagePullPolicy: "Always",
+							Ports: []corev1.ContainerPort{
+								{
+									ContainerPort: 18080,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	if err := r.Create(ctx, &deployment); err != nil {
+		log.Error(err, "创建 Deployment resource 出错")
+		return ctrl.Result{}, err
+	}
 	return ctrl.Result{}, nil
 }
 
